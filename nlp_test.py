9c25f8bd435d9bb9280192f1f089baa5491edbbb
Raymond Ji <raymondji@berkeley.edu>
2020-09-20 13:07:06 -0700

    small fix

--- nlp_test.py
+++ nlp_test.py
@@ -40,32 +40,32 @@ def get_tweets_for_model(cleaned_tokens_list):
 
 def train_model():
 
-    positive_tweets = twitter_samples.strings('positive_tweets.json')
-    negative_tweets = twitter_samples.strings('negative_tweets.json')
-    text = twitter_samples.strings('tweets.20150430-223406.json')
-    #tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
+    # positive_tweets = twitter_samples.strings('positive_tweets.json')
+    # negative_tweets = twitter_samples.strings('negative_tweets.json')
+    # # text = twitter_samples.strings('tweets.20150430-223406.json')
+    # #tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
 
     stop_words = stopwords.words('english')
 
-    positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-    negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
-    positive_cleaned_tokens_list = []
-    negative_cleaned_tokens_list = []
+    # positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+    # negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+    # positive_cleaned_tokens_list = []
+    # negative_cleaned_tokens_list = []
 
-    for tokens in positive_tweet_tokens:
-        positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+    # for tokens in positive_tweet_tokens:
+    #     positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-    for tokens in negative_tweet_tokens:
-        negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+    # for tokens in negative_tweet_tokens:
+    #     negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+    # positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
+    # negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
 
-    positive_dataset = [(tweet_dict, "Positive")
-                         for tweet_dict in positive_tokens_for_model]
+    # positive_dataset = [(tweet_dict, "Positive")
+    #                      for tweet_dict in positive_tokens_for_model]
 
-    negative_dataset = [(tweet_dict, "Negative")
-                         for tweet_dict in negative_tokens_for_model]
+    # negative_dataset = [(tweet_dict, "Negative")
+    #                      for tweet_dict in negative_tokens_for_model]
 
     import json
 
